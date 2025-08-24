989622a7846dc3191cfc96b395760f9b261f5e70
Roberto García <robertogarciacr@gmail.com>
2025-08-24 13:54:17 +0200

    model
    delete import

--- microservice-roles/src/main/java/com/example/rolesAuth/entity/RoleEntity.java
+++ microservice-roles/src/main/java/com/example/rolesAuth/entity/RoleEntity.java
@@ -14,7 +14,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
